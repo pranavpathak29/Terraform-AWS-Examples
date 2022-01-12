64dde9343dde47c824da9835ecf378c588c697b0
Pranav Pathak <pranavpathak29@gmail.com>
2022-01-12 14:05:43 +0530

    change example script

--- aws-kinesis-stream/scripts/script.sh
+++ aws-kinesis-stream/scripts/script.sh
@@ -1,7 +1,7 @@
 #Sample commands for Kinesis Stream
 #To Put Records
-aws kinesis put-record --stream-name ${stream-name} --data ${data} --partition-key ${key}
-aws kinesis put-record --stream-name aws-stream --data "This is Pranav" --partition-key "user1"
+aws kinesis put-record --stream-name ${stream-name} --data ${data} --partition-key ${key} --cli-binary-format raw-in-base64-out
+aws kinesis put-record --stream-name aws-stream --data "This is Pranav" --partition-key "user1" --cli-binary-format raw-in-base64-out
 
 #To Get Shards
 aws kinesis describe-stream --stream-name ${stream-name}
