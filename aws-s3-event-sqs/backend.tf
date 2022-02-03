b3078ac0c27d2d3eb5cab46095cdcc0d66eb1153
Pranav Pathak <pranavpathak29@gmail.com>
2022-02-03 09:31:50 +0530

    renmae s3 event to s3 event sqs

--- aws-s3-event-sqs/backend.tf
+++ aws-s3-event-sqs/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket               = "aws-terraform-stages"
-    key                  = "aws-s3-event"
+    key                  = "aws-s3-event-sqs"
     region               = "ap-south-1"
     workspace_key_prefix = "aws-sample"
   }
